984e8504a06590c18d6851ad564868a205df76f4
Mayank Gupta <mayankguptacse@gmail.com>
2017-07-21 00:00:30 +0530

    Find highest occurrence element.

---
+++ FindHighestOccurenceElement.swift
@@ -0,0 +1,20 @@
+public func findHighestOccurence<T : Hashable>(_ arrayForOperation : [T]) -> (T?, Int){
+    var frequencyHashmap : [T:Int] = [:]
+    var largestFrequency = 0
+    var maxOccuredElement : T? = nil
+    
+    for element in arrayForOperation {
+        if let frequency = frequencyHashmap[element] {
+            frequencyHashmap[element] = frequency + 1
+        }else{
+            frequencyHashmap[element] = 1
+        }
+        
+        if largestFrequency < frequencyHashmap[element]! {
+            largestFrequency = frequencyHashmap[element]!
+            maxOccuredElement = element
+        }
+    }
+    
+    return (maxOccuredElement, largestFrequency)
+}
