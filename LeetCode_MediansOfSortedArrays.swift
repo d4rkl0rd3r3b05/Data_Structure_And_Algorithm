15fb87f12266085d5c9d6dcfaa6795be7f36c4ba
Mayank Gupta <mayankguptacse@gmail.com>
2017-07-28 01:18:46 +0530

    Median of Sorted Arrays made time efficient.

--- LeetCode_MediansOfSortedArrays.swift
+++ LeetCode_MediansOfSortedArrays.swift
@@ -10,12 +10,17 @@ public func getMedianSortedArray(arrays : Array<Int>...) -> Double?{
     var totalElementCount = 0
     arrays.forEach({ totalElementCount = totalElementCount + $0.count})
     
-    func getElementAtIndex(requiredElementIndex : Int) -> Int?{
+    func getElementAtIndex(requiredElementIndex : Int, isSeriesEven : Bool = false) -> Int?{
         var index = 0
         var minCurrentElement : Int?
         var arrayWithMinElementIndex = 0
         var counterArray = Array(repeating: 0, count: arrays.count)
-        while index < requiredElementIndex {
+        while index <= requiredElementIndex {
+            var previousElementInSeries = 0
+            if isSeriesEven && index == requiredElementIndex {
+                previousElementInSeries = arrays[arrayWithMinElementIndex][counterArray[arrayWithMinElementIndex] - 1]
+            }
+            
             minCurrentElement = Int.max
             for currentArrayIndex in 0 ..< arrays.count {
                 if arrays[currentArrayIndex].count <= counterArray[currentArrayIndex]{
@@ -27,6 +32,10 @@ public func getMedianSortedArray(arrays : Array<Int>...) -> Double?{
                 }
             }
             counterArray[arrayWithMinElementIndex] += 1
+            
+            if isSeriesEven && index == requiredElementIndex {
+                minCurrentElement! += previousElementInSeries
+            }
             index += 1
         }
         
@@ -35,8 +44,8 @@ public func getMedianSortedArray(arrays : Array<Int>...) -> Double?{
     
     
     if totalElementCount % 2 == 0 {
-        return Double(getElementAtIndex(requiredElementIndex: totalElementCount/2)! + getElementAtIndex(requiredElementIndex: totalElementCount/2 + 1)!) / 2.0
+        return Double(getElementAtIndex(requiredElementIndex: totalElementCount/2, isSeriesEven: true)!) / 2.0
     }else{
-        return Double(getElementAtIndex(requiredElementIndex: totalElementCount/2 + 1)!)
+        return Double(getElementAtIndex(requiredElementIndex: totalElementCount/2)!)
     }
 }
